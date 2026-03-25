d808cf74db6dd702973bfc6609eea751f4223a8c
Karthick <karthickrajd1@gmail.com>
2026-03-25 18:31:59 +0530

    Added smart switch for pysqlite3

--- appfinal.py
+++ appfinal.py
@@ -1,7 +1,11 @@
-__import__("pysqlite3")
-import sys
+try:
+    __import__("pysqlite3")
+    import sys
 
-sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+except ImportError:
+    # This happens on your Windows machine, which is totally fine!
+    pass
 
 import streamlit as st
 import os
